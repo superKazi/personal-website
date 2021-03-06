31a0d48b4ad599be43c5153f0f646c55ac7bc72c
superKazi <kazi.awal@journalism.cuny.edu>
2021-03-06 16:28:57 -0500

    remove vh css variable

--- src/main.js
+++ src/main.js
@@ -1,48 +1,36 @@
 import { debounce } from "https://cdn.skypack.dev/pin/mini-debounce@v1.0.8-Zdrw8ioDWJBckPavi5e3/min/mini-debounce.js";
 
-// perfect vh
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-window.addEventListener(
-  "resize",
-  debounce(() => {
-    vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }, 300),
-  { passive: true }
-);
-
 // animate bang
 const bang = document.querySelector(".bang");
 
-bang.addEventListener("animationend", () => (bang.style.animationName = ""), {
-  passive: true,
-});
-
-const interactions = [
-  "scroll",
-  "resize",
-  "devicemotion",
-  "deviceorientation",
-  "mousedown",
-  "mousemove",
-  "touchstart",
-  "keydown",
-];
-
-const animations = ["groove", "pop", "swing", "spin"];
-let lastPlayed = "";
-
-interactions.forEach((interaction) =>
-  window.addEventListener(interaction, debounce(animateBang, 1000), {
+if (bang) {
+  bang.addEventListener("animationend", () => (bang.style.animationName = ""), {
     passive: true,
-  })
-);
+  });
 
-function animateBang() {
-  if (window.matchMedia("(orientation: landscape)").matches) {
-    if (bang.style.animationName === "") {
+  const interactions = [
+    "scroll",
+    "resize",
+    "mousedown",
+    "mousemove",
+    "touchstart",
+    "keydown",
+  ];
+
+  interactions.forEach((interaction) =>
+    window.addEventListener(interaction, debounce(animateBang, 1000), {
+      passive: true,
+    })
+  );
+
+  const animations = ["groove", "pop", "swing", "spin"];
+  let lastPlayed = "";
+
+  function animateBang() {
+    if (
+      window.matchMedia("(orientation: landscape)").matches &&
+      bang.style.animationName === ""
+    ) {
       if (lastPlayed === "") {
         bang.style.animationName =
           animations[Math.floor(Math.random() * animations.length)];

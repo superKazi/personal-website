3555e3e78ceaffeb47989838141a20dd135644e9
superKazi <kazi.awal@journalism.cuny.edu>
2021-07-24 14:54:08 -0400

    host files myself

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,9 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     try {
       // https://developers.google.com/web/tools/workbox/modules/workbox-window
-      const lib = await import(
-        'https://storage.googleapis.com/workbox-cdn/releases/6.0.0/workbox-window.prod.mjs'
-      )
-      if (lib.Workbox) {
-        const wb = new lib.Workbox('./sw.js')
+      const { Workbox } = await import('./workbox-window-prod.mjs')
+      if (Workbox) {
+        const wb = new Workbox('../sw.js')
         wb.addEventListener('installed', (event) => {
           if (event.isUpdate) {
             window.location.reload()
@@ -39,15 +37,9 @@ if ('serviceWorker' in navigator) {
 
   if (animationsOn) {
     try {
-      const animeLib = await import(
-        'https://cdn.skypack.dev/pin/animejs@v3.2.1-bxGmKN3J3Mb49M8BrbV5/mode=imports,min/optimized/animejs.js'
-      )
-      const splittingLib = await import(
-        'https://cdn.skypack.dev/pin/splitting@v1.0.6-Za2vDy3XuQ4lO2x5hbUG/mode=imports,min/optimized/splitting.js'
-      )
-      const splittingStyles = await loadCss(
-        'https://unpkg.com/splitting@1.0.6/dist/splitting.css'
-      )
+      const animeLib = await import('./anime.js?v=3.0.2')
+      const splittingLib = await import('./splitting.js?v=3.0.2')
+      const splittingStyles = await loadCss('../styles/splitting.css?v=3.0.2')
 
       if (animeLib.default && splittingLib.default && splittingStyles) {
         const anime = animeLib.default

55b3175d4a451503853844f66b0906c8129b2714
superKazi <kazi.awal@journalism.cuny.edu>
2022-07-13 19:40:55 -0400

    add edge function

---
+++ netlify/edge-functions/geo.js
@@ -0,0 +1,15 @@
+export default async (_, context) => {
+  const response = await context.next();
+  const page = await response.text();
+
+  const regex = /{{geo}}/i;
+
+  const welcomeText =
+    context.geo.city === "Brooklyn" && context.geo.subdivision.code === "NY"
+      ? `from Brooklyn. <em>Kazi</em> built this website there!`
+      : `all the way from ${context.geo.city}! You&#8217;re on <em>Kazi&#8217;s</em> website.`;
+
+  const updatedPage = page.replace(regex, welcomeText);
+
+  return new Response(updatedPage, response);
+};

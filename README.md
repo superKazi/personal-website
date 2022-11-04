b1b459ae2472a73a0d6d95c32fd2ecea861ac44c
superKazi <kazi.awal@journalism.cuny.edu>
2022-11-04 16:42:12 -0400

    back to scrolltrigger

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Netlify builds this code to make my website.
 - Fluid typography and spacing using [Utopia](https://utopia.fyi) by James Gilyead and Trys Mudford
 - Adobe’s [balance text](https://opensource.adobe.com/balance-text/) 
 - Color gradient inspired by [Wado Sanzo | A Dictionary of Color Combinations](https://sanzo-wada.dmbk.io/about)
-- [Motion One](https://motion.dev) for the gradient scroll animation
+- [GSAP ScrollTrigger](https://greensock.com/scrolltrigger/) for the gradient scroll animation
 - Some stuff from this [CSS reset](https://piccalil.li/blog/a-modern-css-reset) by Andy Bell
 - Andrey Sitnik’s [favicon strategy](https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs)
 - [Goatcounter](https://www.goatcounter.com) by Martin Tournoij to track you if/when you visit
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,61 +1,34 @@
 /**
  * Animates the conic gradient on the page on scroll
- * @requires motion
+ * @requires gsap
  * @exports scrollyFun
- * @see {@link https://motion.dev/dom/scroll} for scroll library documentation
+ * @see {@link https://greensock.com/docs/v3/Plugins/ScrollTrigger} for scroll library documentation
  */
 
-import { scroll } from "motion";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 /**
  * @function scrollyFun
- * @description creates scope for lerpedAnimation so it can be exported
+ * @description creates scope for ScrollTrigger animation so it can be exported
  */
+
+gsap.registerPlugin(ScrollTrigger);
+
 function scrollyFun() {
-  let currentY = 0;
-  let targetY = 0;
-
-  /**
-   * @function lerpedAnimation
-   * @description iife that recursively runs via requestAnimationFrame. It listens for vertical scroll and then updates a css custom property using an interpolated value on the next frame.
-   * @see {@link https://easings.net} for easing functions easeOutSine easing function
-   */
-  (function lerpedAnimation() {
-    scroll(({ y }) => {
-      targetY = mapRange(0, 1, clamp(0, y.progress, 1), 0, 360);
-    });
-    const deltaY = targetY - currentY;
-    currentY += deltaY * Math.sin((0.0333 * Math.PI) / 2);
-    document.documentElement.style.setProperty("--deg", `${currentY}deg`);
-
-    requestAnimationFrame(lerpedAnimation);
-  })();
-}
-
-/**
- * @function mapRange
- * @param {number} in_min - the minimum input value
- * @param {number} in_max - the maximum input value
- * @param {number} input - the input value
- * @param {number} out_min - the minimum output value
- * @param {number} out_max - the maxim output value
- * @returns {number} a linear domain range converted value
- * @description taken from {@link https://github.com/studio-freight/lenis}
- */
-function mapRange(in_min, in_max, input, out_min, out_max) {
-  return ((input - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-}
-
-/**
- * @function clamp
- * @param {number} min - the minimum output value
- * @param {number} input - the input value
- * @param {number} max - the maximum output value
- * @returns {number} a value not less than the min or greater than the max
- * @description taken from {@link https://github.com/studio-freight/lenis}
- */
-function clamp(min, input, max) {
-  return Math.max(min, Math.min(input, max));
+  gsap.to("html", {
+    "--deg": "360deg",
+    ease: "sine.out",
+    scrollTrigger: {
+      config: { ignoreMobileResize: true },
+      trigger: "body",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: 4,
+      normalizeScroll: true,
+      invalidateOnRefresh: true,
+    },
+  });
 }
 
 export { scrollyFun };

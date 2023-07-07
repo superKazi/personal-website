e88789f155aa916bb88698a67ffee78bab520566
superKazi <kazi.awal@journalism.cuny.edu>
2023-07-07 16:10:11 -0400

    add back service worker

--- retired.js
+++ retired.js
@@ -1,38 +1,69 @@
-// retire sw
-// async function handleServiceWorker() {
-//   if ("serviceWorker" in navigator) {
-//     try {
-//       // https://developers.google.com/web/tools/workbox/modules/workbox-window
-//       const { Workbox } = await import("workbox-window");
-//       const wb = new Workbox("../sw.js");
-//       wb.addEventListener("installed", (event) => {
-//         if (event.isUpdate) {
-//           window.location.reload();
-//         }
-//       });
-//       wb.register();
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   }
-// }
+// /**
+//  * remove leftover workbox sw stuff
+//  */
 
-// retire vite config with sw
-// export default defineConfig({
-//   root: "./src",
-//   build: {
-//     outDir: "../dist",
-//     emptyOutDir: true,
-//   },
-//   plugins: [
-//     generateSW({
-//       swDest: "./dist/sw.js",
-//       globDirectory: "./dist",
-//       globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg,ico,woff2}"],
-//       sourcemap: false,
-//       cleanupOutdatedCaches: true,
-//       clientsClaim: true,
-//       skipWaiting: true,
-//     }),
-//   ],
-// });
+// (async function killServiceWorkers() {
+//   try {
+//     const registrations = await navigator.serviceWorker.getRegistrations();
+//     const unregisterPromises = registrations.map((registration) =>
+//       registration.unregister(),
+//     );
+
+//     const allCaches = await caches.keys();
+//     const cacheDeletionPromises = allCaches.map((cache) =>
+//       caches.delete(cache),
+//     );
+
+//     const clearAttempted = await Promise.allSettled([
+//       ...unregisterPromises,
+//       ...cacheDeletionPromises,
+//     ]);
+
+//     if (clearAttempted.length > 0) {
+//       if (clearAttempted.includes((p) => p.status === "rejected")) {
+//         throw new Error(
+//           "There was an error clearing google workbox information",
+//         );
+//       } else {
+//         window.location.reload();
+//       }
+//     }
+//   } catch (err) {
+//     console.error(err);
+//   }
+// })();
+
+// (async function clearDbs() {
+//   try {
+//     const dbs = await indexedDB.databases();
+
+//     if (dbs.length > 0) {
+//       dbs.forEach((db) => {
+//         const deletedDb = indexedDB.deleteDatabase(db.name);
+//         deletedDb.onerror = () => {
+//           throw new Error("Couldn’t delete google workbox indexDB");
+//         };
+//       });
+//     }
+//   } catch (err) {
+//     console.error(err);
+//   }
+// })();
+
+// self destructing sw
+// https://github.com/NekR/self-destroying-sw
+
+// self.addEventListener('install', function () {
+//   self.skipWaiting()
+// })
+
+// self.addEventListener('activate', function () {
+//   self.registration
+//     .unregister()
+//     .then(function () {
+//       return self.clients.matchAll()
+//     })
+//     .then(function (clients) {
+//       clients.forEach((client) => client.navigate(client.url))
+//     })
+// })

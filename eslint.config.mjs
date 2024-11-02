9b64fa5c22bf5bdc56cf66d7d136e26552ff844e
superKazi <kazi.awal@journalism.cuny.edu>
2024-11-02 08:54:13 -0400

    update build tool config files

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+  prettier,
+];

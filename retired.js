f7536ca2fa1ba37825b6e637261d1dfc3a489200
Kazi Awal <kazi.digs@gmail.com>
2025-05-10 20:09:04 -0400

    redesign

--- retired.js
+++ retired.js
@@ -67,3 +67,22 @@
 //       clients.forEach((client) => client.navigate(client.url))
 //     })
 // })
+
+//node script to get the colors i want into a json
+// npm i dictionary-of-colour-combinations
+// import colors from "dictionary-of-colour-combinations" with { type: "json" };
+// import { writeFile } from "fs";
+
+// let num = 121;
+// let threeColorCombos = [];
+
+// while (num <= 240) {
+//   threeColorCombos.push(colors.filter((el) => el.combinations.includes(num)));
+//   num = num + 1;
+// }
+
+// const str = JSON.stringify(threeColorCombos);
+
+// writeFile(`${process.cwd()}/src/js/colors.json`, str, (err) =>
+//   console.error(err),
+// );
